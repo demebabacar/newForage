19003071d1b1ca7c0f5fe1b79712ce1ba70ba9ad
dev <demebabacar93@gmail.com>
2019-05-20 20:30:47 +0000

    modifié :         routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Route::get('/test', function () {
 });
 
 Auth::routes();
-route::get('/home', 'HomeController@index')->name('home');
-route::resources('villages', 'VillageController');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::resources('villages', 'VillageController');
